44ad4915d1744d4d7dc86e743ef6facc91419bf4
Shreyansh Tripathy <shreyanshtripathy49@gmail.com>
2025-04-02 01:01:59 +0530

    feat: Refactor CreateProject component to include cancel functionality and improve form handling

--- src/app/(protected)/projects/__components/CreateProject.tsx
+++ src/app/(protected)/projects/__components/CreateProject.tsx
@@ -5,7 +5,6 @@ import {
   SheetHeader,
   SheetTitle,
 } from "@/components/ui/sheet";
-import { Workflow } from "lucide-react";
 import { ICreateProject } from "@/types/project.types";
 import { SubmitHandler } from "react-hook-form";
 import useRefetch from "@/hooks/useRefetch";
@@ -24,31 +23,10 @@ type Props = {
 };
 
 const CreateProject = ({ open, onOpenChange }: Props) => {
-  return (
-    <Sheet open={open} onOpenChange={onOpenChange}>
-      <SheetContent className="flex flex-col gap-6 overflow-y-auto sm:max-w-[420px]">
-        <SheetHeader className="space-y-0">
-          <div className="flex items-center gap-2">
-            <SheetTitle className="flex items-center gap-2 text-base">
-              Add New Project
-            </SheetTitle>
-          </div>
-          <p className="text-sm text-muted-foreground">
-            Create a new project by linking your GitHub repository
-          </p>
-        </SheetHeader>
-        <div className="flex flex-1 flex-col">
-          <CreateProjectPage />
-        </div>
-      </SheetContent>
-    </Sheet>
-  );
-};
-
-const CreateProjectPage = () => {
-  const { register, handleSubmit, reset, formState } = useForm<ICreateProject>({
-    resolver: zodResolver(createProjectValidationSchema),
-  });
+  const { register, handleSubmit, reset, formState, clearErrors } =
+    useForm<ICreateProject>({
+      resolver: zodResolver(createProjectValidationSchema),
+    });
 
   const createProject = api.project.createProject.useMutation();
   const refetch = useRefetch();
@@ -69,70 +47,93 @@ const CreateProjectPage = () => {
     });
   };
 
+  const handleCancel = () => {
+    reset();
+    clearErrors();
+    onOpenChange();
+  };
+
   return (
-    <div className="flex h-full flex-col">
-      <div className="mb-8 flex justify-center">
-        <Image
-          alt="Link GitHub Repository"
-          src="/undraw_developer_activity.svg"
-          width={200}
-          height={200}
-          className="h-auto"
-        />
-      </div>
+    <Sheet open={open} onOpenChange={onOpenChange}>
+      <SheetContent className="flex flex-col gap-6 overflow-y-auto sm:max-w-[420px]">
+        <SheetHeader className="space-y-0">
+          <div className="flex items-center gap-2">
+            <SheetTitle className="flex items-center gap-2 text-base">
+              Add New Project
+            </SheetTitle>
+          </div>
+          <p className="text-sm text-muted-foreground">
+            Create a new project by linking your GitHub repository
+          </p>
+        </SheetHeader>
+        <div className="flex h-full flex-1 flex-col">
+          <div className="mb-8 flex justify-center">
+            <Image
+              alt="Link GitHub Repository"
+              src="/undraw_developer_activity.svg"
+              width={200}
+              height={200}
+              className="h-auto"
+            />
+          </div>
 
-      <div className="mb-6">
-        <h2 className="mb-2 text-xl font-semibold text-primary">
-          Link your GitHub repository
-        </h2>
-        <p className="text-sm text-muted-foreground">
-          Enter the URL of your GitHub repository to get started.
-        </p>
-      </div>
+          <div className="mb-6">
+            <h2 className="mb-2 text-xl font-semibold text-primary">
+              Link your GitHub repository
+            </h2>
+            <p className="text-sm text-muted-foreground">
+              Enter the URL of your GitHub repository to get started.
+            </p>
+          </div>
 
-      <form
-        onSubmit={handleSubmit(onFormSubmit)}
-        className="flex flex-col space-y-5"
-      >
-        <div className="space-y-4">
-          <Input
-            {...register("name")}
-            placeholder="Project Name"
-            className="h-10"
-            description={formState.errors.name?.message}
-            descriptionClassName="mt-1 text-sm text-red-500"
-          />
-
-          <Input
-            {...register("githubUrl")}
-            type="url"
-            placeholder="Repository URL"
-            className="h-10"
-            description={formState.errors.githubUrl?.message}
-            descriptionClassName="mt-1 text-sm text-red-500"
-          />
-
-          <Input
-            {...register("githubToken")}
-            placeholder="Github Token (Optional)"
-            className="h-10"
-            description={formState.errors.githubToken?.message}
-            descriptionClassName="mt-1 text-sm text-red-500"
-          />
-        </div>
-
-        <div className="pt-2">
-          <Button
-            className="px-6"
-            type="submit"
-            disabled={createProject.isPending}
-            size="default"
+          <form
+            onSubmit={handleSubmit(onFormSubmit)}
+            className="flex flex-col space-y-5"
           >
-            {createProject.isPending ? "Creating..." : "Create Project"}
-          </Button>
+            <div className="space-y-4">
+              <Input
+                {...register("name")}
+                placeholder="Project Name"
+                className="h-10"
+                description={formState.errors.name?.message}
+                descriptionClassName="mt-1 text-sm text-red-500"
+              />
+
+              <Input
+                {...register("githubUrl")}
+                type="url"
+                placeholder="Repository URL"
+                className="h-10"
+                description={formState.errors.githubUrl?.message}
+                descriptionClassName="mt-1 text-sm text-red-500"
+              />
+
+              <Input
+                {...register("githubToken")}
+                placeholder="Github Token (Optional)"
+                className="h-10"
+                description={formState.errors.githubToken?.message}
+                descriptionClassName="mt-1 text-sm text-red-500"
+              />
+            </div>
+
+            <div className="grid w-full grid-cols-2 gap-2 pt-2">
+              <Button
+                className="px-6"
+                type="submit"
+                disabled={createProject.isPending}
+                size="default"
+              >
+                {createProject.isPending ? "Creating..." : "Create Project"}
+              </Button>
+              <Button variant="outline" onClick={handleCancel} type="button">
+                Cancel
+              </Button>
+            </div>
+          </form>
         </div>
-      </form>
-    </div>
+      </SheetContent>
+    </Sheet>
   );
 };
 
