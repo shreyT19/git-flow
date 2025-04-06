1c415160050ee912efc5486cd14f743a993970db
Shreyansh Tripathy <shreyanshtripathy49@gmail.com>
2025-04-06 14:56:24 +0530

    feat: Add Stripe integration for creating checkout sessions; enable credit purchases through payment processing

---
+++ src/libs/stripe.libs.ts
@@ -0,0 +1,44 @@
+"use server";
+
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2025-03-31.basil",
+});
+
+export const createCheckoutSession = async (credits: number) => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    throw new Error("User not found");
+  }
+
+  const session = await stripe.checkout.sessions.create({
+    customer: userId,
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: `${credits} GitFlow Credits`,
+          },
+          unit_amount: Math.round(credits / 50) * 100,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: `${process.env.NEXT_PUBLIC_APP_URL}/billing`,
+    cancel_url: `${process.env.NEXT_PUBLIC_APP_URL}/billing`,
+    customer_creation: "always",
+    metadata: {
+      userId,
+      credits,
+    },
+  });
+
+  return redirect(session?.url || "");
+};
